c883ec4c8a21a11fe1158052ccba2a65f8a46588
Sadie Allen <sadiela@bu.edu>
2020-12-22 21:48:53 -0500

    pre pull

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
