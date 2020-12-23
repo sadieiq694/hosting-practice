3f8ecd46fabb159c833b1431cee81de24adde341
Ubuntu <sadiela@bu.edu>
2020-12-23 02:05:14 +0000

    dummy app working!

--- api/app.py
+++ api/app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello there!</h1>"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
